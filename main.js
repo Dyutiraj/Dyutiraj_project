d8a10b25225d476c93966b8394fd22da26e3ddfc
Dyutiraj <109805255+Dyutiraj@users.noreply.github.com>
2022-12-11 21:14:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function slider(anything) {
+    document.querySelector('.one').src = anything
+};
+let menu = document.querySelector('#menu-icons');
+let navbar = document.querySelector(".navbar");
+menu.onclick = () =>
+{
+    menu.classList.toggle('bx-x')
+    navbar.classList.toggle('open')
+}
